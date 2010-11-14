f7147479b7429ba0b1e9322cca3ff4e88be0d149
shiba_yu36 <shibayu36@gmail.com>
2010-11-15 02:20:49 +0900

    投稿して表示する部分まで

--- lib/MojoMvcTutorial/Controller/Entry.pm
+++ lib/MojoMvcTutorial/Controller/Entry.pm
@@ -7,14 +7,47 @@ use base 'MojoMvcTutorial::Controller';
 
 use MojoMvcTutorial::Model;
 
+use FormValidator::Simple;
+
 # This action will render a template
 sub list {
-    my $self = shift;
+    my $c = shift;
 
-    my $entries = MojoMvcTutorial::Model->search('entry',{},{});
+    my @entries = MojoMvcTutorial::Model->search('entry',{},{});
 
     # Render template "example/welcome.html.tx" with message
-    $self->render(entries => $entries);
+    $c->render(entries => \@entries);
+}
+
+sub new_entry {
+    my $c = shift;
+}
+
+sub post {
+    my $c = shift;
+
+    $c->redirect_to('/entry/list') unless $c->req->method eq 'POST';
+
+    # validate
+    my $result = FormValidator::Simple->check($c => [
+        title => ['NOT_BLANK', ['LENGTH', 1, 50]],
+        body  => ['NOT_BLANK', ['LENGTH', 1, 65535]],
+    ]);
+
+    if ($result->has_error) {
+        return $c->render(
+            template => 'entry/new_entry',
+            result   => $result,
+            req      => $c->req,
+        );
+    }
+
+    MojoMvcTutorial::Model->create('entry', {
+        title => $c->param('title'),
+        body  => $c->param('body'),
+    });
+
+    $c->redirect_to('/entry/list');
 }
 
 1;
